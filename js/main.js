b0cf8620c5ea8c1d839c070fe326311248ce738a
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-15 16:55:59 +0100

    feat: arrow up e down funzionanti

--- js/main.js
+++ js/main.js
@@ -1,12 +1,50 @@
+// HTML EL + ARRAY + VARIABILI E COSTANTI
 const slides = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 const slidesContainerEl = document.getElementById("slides-container");
+const arrowUpEl = document.querySelector(".arrow-up");
+const arrowDownEl = document.querySelector(".arrow-down");
 
+let slideIndex = 0;
 let slidesHtml = "";
+
+// CICLO PER CREAZIONE CIMG CON TAMPLATE LITERAL
 for (let i = 0; i < slides.length; i++) {
   const slide = slides[i];
-  let activeClass = i == 0 ? "active" : "";
+  let activeClass = i == slideIndex ? "active" : "";
   slidesHtml += `<img src="./img/${slide}" alt="slide-${i}" class="slide ${activeClass}" />`;
-  console.log(slidesHtml);
 }
 slidesContainerEl.innerHTML = slidesHtml;
-console.log(slidesContainerEl);
+
+// PREMERE FRECCIA SU (AVANTI)
+arrowUpEl.addEventListener("click", function () {
+  // RIMUOVO CLASSE ACTIVE
+  const oldSlide = document.querySelector(".slide.active");
+  oldSlide.classList.remove("active");
+  // INCREMENTO INDICE SLIDE CON CONTROLLO PER FARLO RIPARTIRE
+  if (slideIndex >= slides.length - 1) {
+    slideIndex = 0;
+  } else {
+    slideIndex++;
+  }
+  // RIASSEGNO CLASSE ACTIVE
+  allSlides = document.getElementsByClassName("slide");
+  const newSlides = allSlides[slideIndex];
+  newSlides.classList.add("active");
+});
+
+// PREMERE FRECCIA GIU' (INDIETRO)
+arrowDownEl.addEventListener("click", function () {
+  // RIMUOVO CLASSE ACTIVE
+  const oldSlide = document.querySelector(".slide.active");
+  oldSlide.classList.remove("active");
+  // INCREMENTO INDICE SLIDE CON CONTROLLO PER FARLO RIPARTIRE
+  if (slideIndex <= 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex--;
+  }
+  // RIASSEGNO CLASSE ACTIVE
+  allSlides = document.getElementsByClassName("slide");
+  const newSlides = allSlides[slideIndex];
+  newSlides.classList.add("active");
+});
